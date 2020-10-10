c63784abe1a3d5745e344cd22042c8e8565b74be
EC2 Default User <ec2-user@ip-172-31-72-103.ec2.internal>
2020-10-10 02:17:15 +0000

    Update user model and chatlog model

--- models.py
+++ models.py
@@ -2,27 +2,27 @@
 import flask_sqlalchemy
 from app import db
 
-# class Users(db.Model):
-#     __tablename__='users'
-#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#     userName = db.Column(db.String(20), unique=True, nullable=False)
-#     #userPass = db.Column(db.String(20), unique=True, nullable=False)
-#     def __init__(self,n):
-#         self.userName = n
-#     #    self.userPass = p 
+class Users(db.Model):
+    __tablename__='users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    userName = db.Column(db.String(50), nullable=False) # set unique=True
+    messages = db.relationship("Chatlog", backref="user") # When backref = users, you can get user of a message by doing db_message.user
     
-#     def __repr__(self):
-#         return '<Users userName: %s>' % self.userName 
+    def __init__(self,n):
+        self.userName = n
+    def __repr__(self):
+        return '<Users userName: %s>' % self.userName 
     
 class Chatlog(db.Model):
     __tablename__='chatlog'
     
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String(100))
-#    userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     
-    def __init__(self, m): # Runs the first time a new message is created
+    def __init__(self, m, uid): # Runs the first time a new message is created
         self.message = m
+        self.userId = uid
         
     def __repr__(self): # Represents the object when query'd
         return '<Chatlog message: %s>' % self.message 
