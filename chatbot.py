18c5ff147b61a5852234bc914874388032426c4d
EC2 Default User <ec2-user@ip-172-31-72-103.ec2.internal>
2020-10-12 00:51:12 +0000

    Add last chatbot command

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,8 @@
 import json
 import requests
+import random
 
 
-# Still need one more chatbot command
 class ChatBot:
     
     def __init__(self, botCall):
@@ -13,7 +13,7 @@ class ChatBot:
         if(self.botCall == '!!about'):
             botResponse = "A bot created for this project I am... Type !!help, my commands to see. Yes, hrrmmm."
         elif(self.botCall == '!!help'):
-            botResponse = "!!about !!help !!funtranslate !!quote, the commands I recognize are"
+            botResponse = "!!about !!help !!funtranslate [text] !!quote !!ready, the commands I recognize are"
         elif(self.botCall[0:14] == '!!funtranslate'): # Translate to yoda language https://funtranslations.com/api/yoda
             url = 'https://api.funtranslations.com/translate/yoda.json?text=' + self.botCall[14:]
             response = requests.get(url)
@@ -25,6 +25,12 @@ class ChatBot:
             response = requests.get(url)
             json_body = response.json()
             botResponse = json_body['starWarsQuote']
+        elif(self.botCall == '!!ready'):
+            randomNum = random.randint(0,1000)
+            if(randomNum == 1):
+                botResponse = "This one, a long time have I watched. When gone am I, the last of the Jedi will you be."
+            else:
+                botResponse = "What know you of ready? For eight hundred years have I trained Jedi. To start training Jedi, ready you are not."
         else:
             botResponse = "Recognize that command I do not. All possible commands, type !!help to see"
             
