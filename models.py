800cfecb32853698f4a82d5d329a4bcd9d73bc07
EC2 Default User <ec2-user@ip-172-31-72-103.ec2.internal>
2020-10-12 20:34:33 +0000

    Change models

--- models.py
+++ models.py
@@ -2,16 +2,18 @@
 import flask_sqlalchemy
 from app import db
 
+
 class Users(db.Model):
     __tablename__='users'
+    
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     userName = db.Column(db.String(50), unique=True, nullable=False)
-    messages = db.relationship("Chatlog", backref="user") # When backref = users, you can get user of a message by doing db_message.user
     
     def __init__(self,n):
         self.userName = n
     def __repr__(self):
         return '<Users userName: %s>' % self.userName 
+    
     
 class Chatlog(db.Model):
     __tablename__='chatlog'
@@ -20,9 +22,9 @@ class Chatlog(db.Model):
     message = db.Column(db.String(1000))
     userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     
-    def __init__(self, m, uid): # Runs the first time a new message is created
+    def __init__(self, m, uid):
         self.message = m
         self.userId = uid
         
-    def __repr__(self): # Represents the object when query'd
+    def __repr__(self):
         return '<Chatlog message: %s>' % self.message 
