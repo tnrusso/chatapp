14f42e7364379397b2f7c0ee0a1bcaee0fa32967
EC2 Default User <ec2-user@ip-172-31-72-103.ec2.internal>
2020-10-25 22:00:02 +0000

    Add chatbot tests

--- tests/mocked_unit_tests.py
+++ tests/mocked_unit_tests.py
@@ -1,1 +1,67 @@
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+import os
+
 import unittest
+import unittest.mock as mock
+
+import sys
+sys.path.append('.')
+import app
+from app import app
+from chatbot import ChatBot
+
+import json
+
+KEY_INPUT = "input"
+KEY_EXPECTED = "expected"
+
+
+class MockedFuntranslate:
+    def __init__(self, text):
+        self.text = text
+
+class MockedTestCases(unittest.TestCase):
+    def setUp(self):
+        self.success_test_params = [
+            {
+                KEY_INPUT: "!!funtranslate text",
+                KEY_EXPECTED: "Text"
+            }
+        ]
+
+    
+    def mocked_api_funtranslate(self, botCall):
+        return (
+            {
+                "success": {
+                    "total": 1
+                },
+                "contents": {
+                    "translated": "Text",
+                    "text": "text",
+                    "translation": "yoda"
+                }
+            }    
+        )
+        
+    def mocked_get_request(self, url):
+        return "<Response [200]>"
+        
+        
+    def test_parse_chatbot_message_success(self):
+        for test in self.success_test_params:
+            chatbot = ChatBot(test[KEY_INPUT])
+            mock_get_patcher = mock.patch('requests.get')
+            translated_text = self.mocked_api_funtranslate('')
+            mock_get = mock_get_patcher.start()
+            mock_get.return_value = mock.Mock(status_code = 200)
+            mock_get.return_value.json.return_value = translated_text
+            response = chatbot.get_bot_response()
+            mock_get_patcher.stop()
+            expected = test[KEY_EXPECTED]
+            self.assertEqual(response, expected)
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/unmocked_unit_tests.py
+++ tests/unmocked_unit_tests.py
@@ -1,1 +1,40 @@
 import unittest
+import sys
+sys.path.append('.')
+import app
+from app import app
+from chatbot import ChatBot
+
+KEY_INPUT = "input"
+KEY_EXPECTED = "expected"
+
+class UnmockedTestCases(unittest.TestCase):
+    def setUp(self):
+        self.success_test_params = [
+            {
+                KEY_INPUT: "!!about",
+                KEY_EXPECTED: "A bot created for this project I am... Type !!help, my commands to see. Yes, hrrmmm."
+            },    
+            {
+                KEY_INPUT: "!!help",
+                KEY_EXPECTED: "!!about !!help !!funtranslate [text] !!quote !!ready, the commands I recognize are"
+            },
+            {
+                KEY_INPUT: "!about",
+                KEY_EXPECTED: "Recognize that command I do not. All possible commands, type !!help to see"
+            }
+        ]
+        #self.failure_test_params = []
+
+    def test_parse_message_success(self):
+        for test in self.success_test_params:
+            response = ChatBot(test[KEY_INPUT]).get_bot_response()
+            expected = test[KEY_EXPECTED]
+            self.assertEqual(response, expected)
+            
+#    def test_parse_message_failure(self):
+#        for test in self.failure_test_params:
+#            response = chatbot.get_bot_response(test[KEY_INPUT])
+
+if __name__ == '__main__':
+    unittest.main()
