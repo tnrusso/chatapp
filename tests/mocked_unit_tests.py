a328667e8ab0365ec610ea7ce1d9117c12fd79b7
EC2 Default User <ec2-user@ip-172-31-72-103.ec2.internal>
2020-10-25 22:51:46 +0000

    Chatbot coverage 90%

--- tests/mocked_unit_tests.py
+++ tests/mocked_unit_tests.py
@@ -24,11 +24,34 @@ class MockedFuntranslate:
 
 class MockedTestCases(unittest.TestCase):
     def setUp(self):
-        self.success_test_params = [
+        
+        self.success_quote_params = [
+            {
+                KEY_INPUT: "!!quote",
+                KEY_EXPECTED: "The Force will be with you. Always. — Obi-Wan Kenobi"
+            }    
+        ]
+        
+        
+        self.success_translate_params = [
             {
                 KEY_INPUT: "!!funtranslate text",
                 KEY_EXPECTED: "Text"
             }
+        ]
+        
+        self.failure_translate_params = [
+            {
+                KEY_INPUT: "!!funtranslate text",
+                KEY_EXPECTED: "some incorrect translated text"
+            }
+        ]
+        
+        self.success_translate_length_too_long = [
+            {
+                KEY_INPUT: "!!funtranslate translated text will be too long to return",
+                KEY_EXPECTED: "Too long, your translation is. Please shorten it and try again. Yrsssss."
+            }    
         ]
 
     
@@ -45,13 +68,36 @@ class MockedTestCases(unittest.TestCase):
                 }
             }    
         )
+    
+    def mocked_api_funtranslate_length(self, botCall):
+        length1000 = "i"
+        for i in range(0,1000):
+            length1000 += str(i)
+        return (
+            {
+                "success": {
+                    "total": 1
+                },
+                "contents": {
+                    "translated": length1000,
+                    "text": "translated text will be too long to return",
+                    "translation": "yoda"
+                }
+            }    
+        )
+    
+    def mocked_api_quote(self):
+        return (
+            {
+                "id": 23,
+                "starWarsQuote": "The Force will be with you. Always. — Obi-Wan Kenobi",
+                "faction": 0
+            }    
+        )
+
         
-    def mocked_get_request(self, url):
-        return "<Response [200]>"
-        
-        
-    def test_parse_chatbot_message_success(self):
-        for test in self.success_test_params:
+    def test_chatbot_translate(self):
+        for test in self.success_translate_params:
             chatbot = ChatBot(test[KEY_INPUT])
             mock_get_patcher = mock.patch('requests.get')
             translated_text = self.mocked_api_funtranslate('')
@@ -63,5 +109,46 @@ class MockedTestCases(unittest.TestCase):
             expected = test[KEY_EXPECTED]
             self.assertEqual(response, expected)
 
+    def test_chatbot_translate_failure(self):
+        for test in self.failure_translate_params:
+            chatbot = ChatBot(test[KEY_INPUT])
+            mock_get_patcher = mock.patch('requests.get')
+            translated_text = self.mocked_api_funtranslate('')
+            mock_get = mock_get_patcher.start()
+            mock_get.return_value = mock.Mock(status_code = 200)
+            mock_get.return_value.json.return_value = translated_text
+            response = chatbot.get_bot_response()
+            mock_get_patcher.stop()
+            expected = test[KEY_EXPECTED]
+            self.assertNotEqual(response, expected)
+
+    def test_chatbot_quote_success(self):
+        for test in self.success_quote_params:
+            chatbot = ChatBot(test[KEY_INPUT])
+            mock_get_patcher = mock.patch('requests.get')
+            quote = self.mocked_api_quote()
+            mock_get = mock_get_patcher.start()
+            mock_get.return_value = mock.Mock(status_code = 200)
+            mock_get.return_value.json.return_value = quote
+            response = chatbot.get_bot_response()
+            mock_get_patcher.stop()
+            expected = test[KEY_EXPECTED]
+            self.assertEqual(response, expected)
+            
+    def test_chatbot_translate_length(self):
+        for test in self.success_translate_length_too_long:
+            chatbot = ChatBot(test[KEY_INPUT])
+            mock_get_patcher = mock.patch('requests.get')
+            translated_text = self.mocked_api_funtranslate_length('')
+            mock_get = mock_get_patcher.start()
+            mock_get.return_value = mock.Mock(status_code = 200)
+            mock_get.return_value.json.return_value = translated_text
+            response = chatbot.get_bot_response()
+            mock_get_patcher.stop()
+            expected = test[KEY_EXPECTED]
+            self.assertEqual(response, expected)
+    
+    
+    
 if __name__ == '__main__':
     unittest.main()
--- tests/unmocked_unit_tests.py
+++ tests/unmocked_unit_tests.py
@@ -7,10 +7,10 @@ from chatbot import ChatBot
 
 KEY_INPUT = "input"
 KEY_EXPECTED = "expected"
-
+KEY_EXPECTED_ALT = "conditional_expected"
 class UnmockedTestCases(unittest.TestCase):
     def setUp(self):
-        self.success_test_params = [
+        self.success_chatbot_params = [
             {
                 KEY_INPUT: "!!about",
                 KEY_EXPECTED: "A bot created for this project I am... Type !!help, my commands to see. Yes, hrrmmm."
@@ -22,19 +22,40 @@ class UnmockedTestCases(unittest.TestCase):
             {
                 KEY_INPUT: "!about",
                 KEY_EXPECTED: "Recognize that command I do not. All possible commands, type !!help to see"
+            },
+            {
+                KEY_INPUT: "!!",
+                KEY_EXPECTED: "Recognize that command I do not. All possible commands, type !!help to see"
+            },
+            {
+                KEY_INPUT: "!! help",
+                KEY_EXPECTED: "Recognize that command I do not. All possible commands, type !!help to see"
             }
         ]
-        #self.failure_test_params = []
+        
+        self.success_ready_params = [
+            {
+                KEY_INPUT: "!!ready",
+                KEY_EXPECTED: "What know you of ready? For eight hundred years have I trained Jedi. To start training Jedi, ready you are not.",
+                KEY_EXPECTED_ALT: "This one, a long time have I watched. When gone am I, the last of the Jedi will you be."
+            }    
+        ]
 
+    
     def test_parse_message_success(self):
-        for test in self.success_test_params:
+        for test in self.success_chatbot_params:
             response = ChatBot(test[KEY_INPUT]).get_bot_response()
             expected = test[KEY_EXPECTED]
             self.assertEqual(response, expected)
-            
-#    def test_parse_message_failure(self):
-#        for test in self.failure_test_params:
-#            response = chatbot.get_bot_response(test[KEY_INPUT])
+          
+    def test_success_ready_params(self):
+        for test in self.success_ready_params:
+            response = ChatBot(test[KEY_INPUT]).get_bot_response()
+            expected = test[KEY_EXPECTED]
+            expected_alt = test[KEY_EXPECTED_ALT]
+            self.assertTrue(response == expected or response == expected_alt)
+                   
+
 
 if __name__ == '__main__':
     unittest.main()
