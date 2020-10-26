99a6d9f37b872b22bbd1b6cded516a7da9223a85
EC2 Default User <ec2-user@ip-172-31-72-103.ec2.internal>
2020-10-26 14:25:22 +0000

    Clean up models code

--- models.py
+++ models.py
@@ -1,37 +1,41 @@
-# https://flask-sqlalchemy.palletsprojects.com/en/2.x/models/
-import flask_sqlalchemy
+'''This module creates the user and chatlog database models'''
+# pylint: disable=no-member, too-few-public-methods
+#import flask_sqlalchemy
 from app import db
 
 
 class Users(db.Model):
-    __tablename__='users'
-    
+    '''Model for registered users'''
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    userName = db.Column(db.String(100), nullable=False)
-    userEmail = db.Column(db.String(1000), unique=True, nullable=False)
-    userAvatar = db.Column(db.String(1000), nullable=False)
-    sessionID = db.Column(db.String(100))
-    
-    def __init__(self, n, e, a, s):
-        self.userName = n
-        self.userEmail = e
-        self.userAvatar = a
-        self.sessionID = s
-        
+    user_name = db.Column(db.String(100), nullable=False)
+    user_email = db.Column(db.String(1000), unique=True, nullable=False)
+    user_avatar = db.Column(db.String(1000), nullable=False)
+    session_id = db.Column(db.String(100))
+
+    def __init__(self, name, email, avatar, sid):
+        self.user_name = name
+        self.user_email = email
+        self.user_avatar = avatar
+        self.session_id = sid
+
     def __repr__(self):
-        return '<Users userName: %s>' % self.userName 
-    
-    
+        return '<Users userName: %s>' % self.user_name
+
+
 class Chatlog(db.Model):
-    __tablename__='chatlog'
-    
+    '''Model of chat messages'''
+    __tablename__ = 'chatlog'
+
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String(1000))
-    userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    
-    def __init__(self, m, uid):
-        self.message = m
-        self.userId = uid
-        
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __init__(self, message, uid):
+        self.message = message
+        self.user_id = uid
+
     def __repr__(self):
-        return '<Chatlog message: %s>' % self.message 
+        return '<Chatlog message: %s>' % self.message
+    
