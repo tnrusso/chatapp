b519ab43399d2626526392941ed4a2d22e817180
EC2 Default User <ec2-user@ip-172-31-72-103.ec2.internal>
2020-10-27 00:22:48 +0000

    Update test files

--- tests/mocked_unit_tests.py
+++ tests/mocked_unit_tests.py
@@ -7,9 +7,18 @@ import unittest
 import unittest.mock as mock
 
 import sys
-sys.path.append('.')
+
+sys.path.append(".")
 import app
-from app import emit_all_messages, add_user, update_user_count, successful_google_login
+from app import (
+    emit_all_messages,
+    add_user,
+    update_user_count,
+    successful_google_login,
+    on_new_message,
+    on_connect,
+    on_disconnect,
+)
 from app import app
 from chatbot import ChatBot
 import models
@@ -30,153 +39,169 @@ KEY_EXPECTED = "expected"
 KEY_USER = "name"
 KEY_EMAIL = "email"
 KEY_AVATAR = "avatar"
-KEY_SESSION_ID = "sessionID"
+KEY_SESSION_ID = "session_id"
 KEY_MESSAGE = "message"
 KEY_ID = "id"
 
-class MockedModelsUsers:
-    def __init__(self, userName, userEmail, userAvatar, sessionID):
-        self.userName = userName
-        self.userEmail = userEmail
-        self.userAvatar = userAvatar
-        self.sessionID = sessionID
 
-class MockedModelsChatlog:
+class mocked_models_users:
+    def __init__(self, user_name, user_email, user_avatar, session_id):
+        self.user_name = user_name
+        self.user_email = user_email
+        self.user_avatar = user_avatar
+        self.session_id = session_id
+
+
+class mocked_models_chatlog:
     def __init__(self, message, userID):
         self.message = message
         self.userID = userID
 
+
 class MockedTestCases(unittest.TestCase):
     def setUp(self):
-        self.app = Flask(__name__)
-        self.socketio = flask_socketio.SocketIO(app)
 
         self.success_quote_params = [
             {
                 KEY_INPUT: "!!quote",
-                KEY_EXPECTED: "The Force will be with you. Always. — Obi-Wan Kenobi"
-            }    
-        ]
-        
-        self.success_translate_params = [
-            {
-                KEY_INPUT: "!!funtranslate text",
-                KEY_EXPECTED: "Text"
+                KEY_EXPECTED: "The Force will be with you. Always. — Obi-Wan Kenobi",
             }
         ]
-        
+
+        self.success_translate_params = [
+            {KEY_INPUT: "!!funtranslate text", KEY_EXPECTED: "Text"}
+        ]
+
         self.failure_translate_params = [
             {
                 KEY_INPUT: "!!funtranslate text",
-                KEY_EXPECTED: "some incorrect translated text"
+                KEY_EXPECTED: "some incorrect translated text",
             }
         ]
-        
+
         self.success_translate_length_too_long = [
             {
                 KEY_INPUT: "!!funtranslate translated text will be too long to return",
-                KEY_EXPECTED: "Too long, your translation is. Please shorten it and try again. Yrsssss."
-            }    
+                KEY_EXPECTED: "Too long, your translation is. Please shorten it and try again. Yrsssss.",
+            }
         ]
-        
+
         self.success_add_user_params = [
             {
-                KEY_USER:"My Name",
-                KEY_EMAIL:"useremail@gmail.com",
+                KEY_USER: "My Name",
+                KEY_EMAIL: "user_email@gmail.com",
                 KEY_AVATAR: "avatar",
-                KEY_SESSION_ID: "session id"
+                KEY_SESSION_ID: "session id",
             },
             {
                 KEY_USER: "Users Full Name",
                 KEY_EMAIL: "hello@gmail.com",
                 KEY_AVATAR: "avatar",
-                KEY_SESSION_ID: "session id" 
-            }
+                KEY_SESSION_ID: "session id",
+            },
         ]
-        
-        self.success_add_message_params = [
-            {
-                KEY_MESSAGE: "message text",
-                KEY_ID: 1
-            }    
-        ]
-        
-        self.success_socket_on = [
+
+        self.success_add_message_params = [{KEY_MESSAGE: "message text", KEY_ID: 1}]
+
+        self.success_socket_user = [
             {
                 KEY_INPUT: "new google user",
                 KEY_EXPECTED: {
-                    'name': "new google user",
-                    'email': 'email@email.com',
-                    'avatar': 'avatar'
-                }
-            }    
-        ]
-        
-        
-    
-    def mocked_api_funtranslate(self, botCall):
-        return (
-            {
-                "success": {
-                    "total": 1
+                    "name": "new google user",
+                    "email": "email@email.com",
+                    "avatar": "avatar",
                 },
-                "contents": {
-                    "translated": "Text",
-                    "text": "text",
-                    "translation": "yoda"
-                }
-            }    
-        )
-    
+            }
+        ]
+
+        self.success_add_user_method = [
+            {
+                KEY_USER: "name",
+                KEY_EMAIL: "email@gmail.com",
+                KEY_AVATAR: "avatar",
+                KEY_EXPECTED: None,
+            },
+            {
+                KEY_USER: "qwertqwy",
+                KEY_EMAIL: "my_email@gmail.com",
+                KEY_AVATAR: "avatar",
+                KEY_EXPECTED: None,
+            },
+            {
+                KEY_USER: "My Username",
+                KEY_EMAIL: "bug@gmail.com",
+                KEY_AVATAR: "avatar",
+                KEY_EXPECTED: None,
+            },
+        ]
+
+    def mocked_api_funtranslate(self, botCall):
+        return {
+            "success": {"total": 1},
+            "contents": {"translated": "Text", "text": "text", "translation": "yoda"},
+        }
+
     def mocked_api_funtranslate_length(self, botCall):
         length1000 = "i"
-        for i in range(0,1000):
+        for i in range(0, 1000):
             length1000 += str(i)
-        return (
-            {
-                "success": {
-                    "total": 1
-                },
-                "contents": {
-                    "translated": length1000,
-                    "text": "translated text will be too long to return",
-                    "translation": "yoda"
-                }
-            }    
-        )
-    
+        return {
+            "success": {"total": 1},
+            "contents": {
+                "translated": length1000,
+                "text": "translated text will be too long to return",
+                "translation": "yoda",
+            },
+        }
+
     def mocked_api_quote(self):
-        return (
-            {
-                "id": 23,
-                "starWarsQuote": "The Force will be with you. Always. — Obi-Wan Kenobi",
-                "faction": 0
-            }    
-        )
+        return {
+            "id": 23,
+            "starWarsQuote": "The Force will be with you. Always. — Obi-Wan Kenobi",
+            "faction": 0,
+        }
 
     def mocked_add_user(self, name, email, avatar):
-        return MockedModelsUsers(name, email, avatar, 123)
-        
-    def mocked_add_new_message(self,message,uid):
-        return MockedModelsChatlog(message,uid)
-    
+        return mocked_models_users(name, email, avatar, 123)
+
+    def mocked_add_new_message(self, message, uid):
+        return mocked_models_chatlog(message, uid)
+
+    def mocked_add_bot(self):
+        bot = mocked_models_users("name", "email", "avatar", "1")
+        return bot
+
+    def mocked_db_query(self, name):
+        return None
+
     def test_add_user_to_db(self):
         for test in self.success_add_user_params:
-            with mock.patch('models.db.session.add', self.mocked_add_user):
-                response = models.Users(test[KEY_USER], test[KEY_EMAIL], test[KEY_AVATAR], test[KEY_SESSION_ID])
-                
+            with mock.patch("models.db.session.add", self.mocked_add_user):
+                models.Users(
+                    test[KEY_USER],
+                    test[KEY_EMAIL],
+                    test[KEY_AVATAR],
+                    test[KEY_SESSION_ID],
+                )
+                expected = self.mocked_add_user(
+                    test[KEY_USER], test[KEY_EMAIL], test[KEY_AVATAR]
+                )
+                self.assertIsInstance(expected, mocked_models_users)
+
     def test_add_message_to_db(self):
         for test in self.success_add_message_params:
-            with mock.patch('models.db.session.add', self.mocked_add_new_message):
-                response = models.Chatlog(test[KEY_MESSAGE], test[KEY_ID])
-    
+            with mock.patch("models.db.session.add", self.mocked_add_new_message):
+                models.Chatlog(test[KEY_MESSAGE], test[KEY_ID])
+                expected = self.mocked_add_new_message(test[KEY_MESSAGE], test[KEY_ID])
+                self.assertIsInstance(expected, mocked_models_chatlog)
+
     def test_chatbot_translate(self):
         for test in self.success_translate_params:
             chatbot = ChatBot(test[KEY_INPUT])
-            mock_get_patcher = mock.patch('requests.get')
-            translated_text = self.mocked_api_funtranslate('')
+            mock_get_patcher = mock.patch("requests.get")
+            translated_text = self.mocked_api_funtranslate("")
             mock_get = mock_get_patcher.start()
-            mock_get.return_value = mock.Mock(status_code = 200)
+            mock_get.return_value = mock.Mock(status_code=200)
             mock_get.return_value.json.return_value = translated_text
             response = chatbot.get_bot_response()
             mock_get_patcher.stop()
@@ -186,10 +211,10 @@ class MockedTestCases(unittest.TestCase):
     def test_chatbot_translate_failure(self):
         for test in self.failure_translate_params:
             chatbot = ChatBot(test[KEY_INPUT])
-            mock_get_patcher = mock.patch('requests.get')
-            translated_text = self.mocked_api_funtranslate('')
+            mock_get_patcher = mock.patch("requests.get")
+            translated_text = self.mocked_api_funtranslate("")
             mock_get = mock_get_patcher.start()
-            mock_get.return_value = mock.Mock(status_code = 200)
+            mock_get.return_value = mock.Mock(status_code=200)
             mock_get.return_value.json.return_value = translated_text
             response = chatbot.get_bot_response()
             mock_get_patcher.stop()
@@ -199,35 +224,47 @@ class MockedTestCases(unittest.TestCase):
     def test_chatbot_quote_success(self):
         for test in self.success_quote_params:
             chatbot = ChatBot(test[KEY_INPUT])
-            mock_get_patcher = mock.patch('requests.get')
+            mock_get_patcher = mock.patch("requests.get")
             quote = self.mocked_api_quote()
             mock_get = mock_get_patcher.start()
-            mock_get.return_value = mock.Mock(status_code = 200)
+            mock_get.return_value = mock.Mock(status_code=200)
             mock_get.return_value.json.return_value = quote
             response = chatbot.get_bot_response()
             mock_get_patcher.stop()
             expected = test[KEY_EXPECTED]
             self.assertEqual(response, expected)
-            
+
     def test_chatbot_translate_length(self):
         for test in self.success_translate_length_too_long:
             chatbot = ChatBot(test[KEY_INPUT])
-            mock_get_patcher = mock.patch('requests.get')
-            translated_text = self.mocked_api_funtranslate_length('')
+            mock_get_patcher = mock.patch("requests.get")
+            translated_text = self.mocked_api_funtranslate_length("")
             mock_get = mock_get_patcher.start()
-            mock_get.return_value = mock.Mock(status_code = 200)
+            mock_get.return_value = mock.Mock(status_code=200)
             mock_get.return_value.json.return_value = translated_text
             response = chatbot.get_bot_response()
             mock_get_patcher.stop()
             expected = test[KEY_EXPECTED]
             self.assertEqual(response, expected)
-        
+
     @mock.patch("flask_socketio.SocketIO.emit")
     def test_socket_emit(self, emit_message):
-        for test in self.success_socket_on:
+        for test in self.success_socket_user:
             with mock.patch("app.add_user", self.mocked_add_user):
-                self.socketio.emit(test[KEY_INPUT], test[KEY_EXPECTED])
                 successful_google_login(test[KEY_EXPECTED])
-    
-if __name__ == '__main__':
+                expected = self.mocked_add_user(
+                    test[KEY_EXPECTED]["name"],
+                    test[KEY_EXPECTED]["email"],
+                    test[KEY_EXPECTED]["avatar"],
+                )
+                self.assertIsInstance(expected, mocked_models_users)
+
+    @mock.patch("flask_socketio.SocketIO.emit")
+    def test_socket_connect(self, emit_message):
+        with mock.patch("app.add_bot", self.mocked_add_bot):
+            on_connect()
+            self.assertEqual(on_connect(), None)
+
+
+if __name__ == "__main__":
     unittest.main()
--- tests/unmocked_unit_tests.py
+++ tests/unmocked_unit_tests.py
@@ -2,7 +2,7 @@ import unittest
 import sys
 sys.path.append('.')
 import app
-from app import app
+from app import app, bot_command_called
 from chatbot import ChatBot
 
 KEY_INPUT = "input"
@@ -33,6 +33,29 @@ class UnmockedTestCases(unittest.TestCase):
             }
         ]
         
+        self.failure_chatbot_params = [
+            {
+                KEY_INPUT: "!!about",
+                KEY_EXPECTED: "!!about !!help !!funtranslate [text] !!quote !!ready, the commands I recognize are"
+            },    
+            {
+                KEY_INPUT: "!!help",
+                KEY_EXPECTED: "the commands I recognize are"
+            },
+            {
+                KEY_INPUT: "!about",
+                KEY_EXPECTED: "A bot created for this project I am..."
+            },
+            {
+                KEY_INPUT: "!!",
+                KEY_EXPECTED: "!!about !!help !!funtranslate [text] !!quote !!ready, the commands I recognize are"
+            },
+            {
+                KEY_INPUT: "!! help",
+                KEY_EXPECTED: "!!about !!help !!funtranslate [text] !!quote !!ready, the commands I recognize are"
+            }
+        ]
+        
         self.success_ready_params = [
             {
                 KEY_INPUT: "!!ready",
@@ -40,22 +63,47 @@ class UnmockedTestCases(unittest.TestCase):
                 KEY_EXPECTED_ALT: "This one, a long time have I watched. When gone am I, the last of the Jedi will you be."
             }    
         ]
+        
+        self.success_bot_command_call = [
+            {
+                KEY_INPUT: "!!about",
+                KEY_EXPECTED: None
+            },
+            {
+                KEY_INPUT: "!!quote",
+                KEY_EXPECTED: None
+            },
+            {
+                KEY_INPUT: "!!help",
+                KEY_EXPECTED: None
+            }
+                
+        ]
 
     
-    def test_parse_message_success(self):
+    def test_success_bot_message(self):
         for test in self.success_chatbot_params:
             response = ChatBot(test[KEY_INPUT]).get_bot_response()
             expected = test[KEY_EXPECTED]
             self.assertEqual(response, expected)
-          
+
+    def test_failure_bot_message(self):
+        for test in self.failure_chatbot_params:
+            response = ChatBot(test[KEY_INPUT]).get_bot_response()
+            expected = test[KEY_EXPECTED]
+            self.assertNotEqual(response, expected)
+
     def test_success_ready_params(self):
         for test in self.success_ready_params:
             response = ChatBot(test[KEY_INPUT]).get_bot_response()
             expected = test[KEY_EXPECTED]
             expected_alt = test[KEY_EXPECTED_ALT]
             self.assertTrue(response == expected or response == expected_alt)
-                   
 
+    def test_bot_command_call(self):
+        for test in self.success_bot_command_call:
+            expected = test[KEY_EXPECTED]
+            self.assertEqual(bot_command_called(test[KEY_INPUT]), expected)
 
 if __name__ == '__main__':
     unittest.main()
